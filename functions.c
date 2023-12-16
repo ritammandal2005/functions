9675a55aab6109eeefb015f21e7c10f86b7d4109
RitamMandal <151548534+ritammandal2005@users.noreply.github.com>
2023-12-16 11:23:40 +0530

    Add files via upload

--- functions.c
+++ functions.c
@@ -1,25 +1,22 @@
 #include<stdio.h>
-void namaste();
-void bonjour();
-int main()
-{
-    printf("Enter i for india & f for french :");
-    char ch;
-    scanf("%c",&ch);
-    if(ch =='i') {
-        namaste();
-    }
-    else {
-    bonjour();
-    }
- 
- return 0;
+
+int fib(int n);
+
+int main (){
+printf("%d",fib(6));
+return 0;
 }
 
-void namaste() {
-    printf("Namaste \n");
-}
-
-void bonjour() {
-    printf("Bonjour \n");
+int fib(int n){
+        if(n == 0){
+        return 0;
+    }
+    if(n==1){
+        return 1;
+    }
+    
+    int fibNm1 = fib(n-1);
+    int fibNm2 = fib(n-2);
+    int fibN = fibNm1 + fibNm2;
+    return fibN;
 }
